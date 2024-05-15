4bb271d2d3a5f270063505d63d0b7afc73502418
Arnold Melet <arnoldmelet18@gmail.com>
2024-05-15 17:15:00 +0200

    listo

--- main.js
+++ main.js
@@ -70,7 +70,9 @@ const pokemonmerge = { ...pokemon, ...pikachu}
 
 console.log(pokemonmerge); //se combinan los valores iguales
 
-let suma = 0
+
+
+
 function sumEveryOther(...n){
     let total = n.reduce((a, b) => a + b, 0);
 
@@ -78,16 +80,85 @@ function sumEveryOther(...n){
 
 }
 
-
-
 sumEveryOther(6, 8, 2, 3, 1); //20
 sumEveryOther(11, 3, 12); //26 
 
 
 
-function addOnlyNums (...arr){
-    if ((typeof(arr[1]) == Number)) {
-        
+function addOnlyNums(...arr){
+    let sum=0
+    for (let i=0; i<arr.length;i++){
+        if(typeof arr[i] === "number"){
+            sum+=arr[i]
+        }
     }
-    let total = n.reduce((a, b) => a + b, 0);
+    console.log(sum);
+    return sum
 }
+
+addOnlyNums (1, 'perro', 2, 4); //7
+
+
+
+function countTheArgs(...countargs){
+   console.log(countargs.length)
+}
+
+countTheArgs('gato', 'perro') //2
+countTheArgs('gato', 'perro', 'pollo', 'oso') //4
+
+const n1=[1,2,3,4]
+const n2=[10,12]
+combineTwoArrays(n1,n2)
+
+function combineTwoArrays(arr1,arr2){
+   let arrayspread = [...arr1,...arr2]
+console.log(arrayspread)
+} 
+
+
+
+//EXTRAS
+
+const HIGH_TEMPERATURES = {
+    yesterday: 30,
+    today: 35,
+    tomorrow: 32,
+  };
+
+  const maximaHoy = HIGH_TEMPERATURES.today;
+const maximaManana = HIGH_TEMPERATURES.tomorrow;
+console.log(maximaHoy)
+console.log(maximaManana)
+
+  
+
+onlyUniques('gato', 'pollo', 'cerdo', 'cerdo'); //['gato', 'pollo', 'cerdo']
+onlyUniques(1, 1, 2, 2, 3, 6, 7, 8); //[1, 2, 3, 6, 7, 8]
+
+function onlyUniques (...a){
+    const norepetidos = new Set(a);
+   console.log(Array.from(norepetidos)) 
+}
+
+
+
+combineAllArrays([3, 6, 7, 8],[2, 7, 3, 1])
+// [3, 6, 7, 8, 2, 7, 3, 1]
+combineAllArrays([2, 7, 3, 1],[2, 7, 4, 12],[2, 44, 22, 7, 3, 1]);
+// [2, 7, 3, 1, 2, 7, 4, 12, 2, 44, 22, 7, 3, 1]
+
+function combineAllArrays(...arrays) {
+    
+    console.log( [].concat(...arrays));}
+
+
+
+    function sumAndSquare(...args) {
+        return args.map(num => num * num).reduce((sum, squared) => sum + squared, 0);
+      }
+      
+      console.log(sumAndSquare(3, 6, 7, 8,2, 7, 3, 1)); 
+      console.log(sumAndSquare(4, 5));    
+      console.log(sumAndSquare(2, 7, 3, 1));
+      
